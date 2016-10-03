0f076f2dc4365af9d6dde3badc48afb6010e087e
Harry N <harry.nguyentrongdat@gmail.com>
2016-10-03 20:02:52 +0700

    validate title & body of article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ActiveRecord::Base
+	validates :title, :presence => { :message => '| Don\'t leave title empty!' }
+	validates :body, :presence => { :message => '| Don\'t leave body empty!' }
+	validates :title, :length => { :minimum => 2, :message => '| Please set a longer title (at least 3 characters)!' }
+	validates :title, :uniqueness => { :message => '| This title is taken already. Please choose another one!' }
+	validates :body, :length => { :minimum => 10, :message => '| Please set a longer body (at least 10 characters)!'  }
 end
